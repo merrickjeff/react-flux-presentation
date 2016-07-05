b49bc366aa0d0322eca6f79d2cdd05d873e680ff
Jeff Merrick <JMerrick@ExtendHealth.com>
2016-07-05 13:02:49 -0600

    Updated dependencies, started adding react components

---
+++ src/components/main.jsx
@@ -0,0 +1,18 @@
+"use strict";
+
+var React = require('react');
+
+var Main = React.createClass({
+	render: function(){
+		return (
+			<div className="jumbotron">
+				<h1>This is the Main component</h1>
+				<p>What do you think? This is the main component p content</p>
+			</div>
+
+			);
+	}
+
+});
+
+module.exports = Main;
--- src/main.js
+++ src/main.js
@@ -2,7 +2,18 @@
 
 var $, jQuery;
 $ = jQuery = require('jquery');
+var React = require('react');
+var ReactDOM = require('react-dom');
+var Main = require('./components/main.jsx');
 
 var App = console.log('This function is wired up to log from main.js');
+var App = function(){
+	let stringggg = 'Dis function is wired up to log from main.js';
+	console.log(stringggg);
+
+}();
+
+
+ReactDOM.render(<Main/>, document.getElementById('app'));
 
 
